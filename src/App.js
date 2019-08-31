7939f751c10d8561501f15cd9b520f2e3b42f550
Domas Žičevičius <domas.zicevicius@gmail.com>
2019-08-31 15:33:15 +0300

    Sign-in and some generic components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInSignUpPage from './pages/signin-signup/signin-signup.component';
 import Header from './components/header/header.component';
 
 
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUpPage} />
       </Switch>
     </div>
   );
