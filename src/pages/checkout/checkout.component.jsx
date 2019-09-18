50aedde3c5f7779476a49e03bfd3ccaa37304076
Domas Žičevičius <domas.zicevicius@gmail.com>
2019-09-18 22:37:05 +0300

    List checkout items in checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
@@ -27,7 +29,9 @@ const CheckoutPage = ({ cartItems, total }) => (
             </div>
         </div>
         {
-            cartItems.map(cartItem => cartItem.name)
+            cartItems.map(cartItem => (
+                <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+            ))
         }
         <div className='total'>
             <span>TOTAL: ${total}</span>
@@ -40,7 +44,4 @@ const mapStateToProps = createStructuredSelector({
     total: selectCartTotal
 });
 
-export default connect(
-    mapStateToProps,
-    null
-)(CheckoutPage);    
+export default connect(mapStateToProps)(CheckoutPage);    
