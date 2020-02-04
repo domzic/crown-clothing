2342769bf5d35e87ef3f836d7c9e340999bd497d
Domas Žičevičius <domas.zicevicius@gmail.com>
2020-02-04 23:04:21 +0200

    Create node server

--- client/src/pages/collection/collection.container.jsx
+++ client/src/pages/collection/collection.container.jsx
@@ -2,12 +2,12 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
-import { selectIsCollectionLoaded } from '../../redux/shop/shop.selectors';
+import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 import CollectionPage from './collection.component';
 
 const mapStateToProps = createStructuredSelector({
-    isLoading: state => !selectIsCollectionLoaded(state)
+    isLoading: state => !selectIsCollectionFetching(state)
 });
 
 const CollectionPageContainer = compose(
